73857f626f2a86dd28dbc656bdf26f5daacb7872
Ali A. Jafari <aajafari87@gmail.com>
2021-10-15 16:30:45 +0330

    add eslint config file

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.scss";
 
-function App() {
-  return <div className="App">React Project with Webpack</div>;
-}
+const App = () => {
+  return <div>React </div>;
+};
 
 export default App;
